032935279b91862fa3cc07d0727fe9dab3c817cf
Jesu <jesusdiaz013@gmail.com>
2024-07-03 07:46:39 -0300

    html para inicio

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ app.use('/products', productsRoutes);
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res)=> {
+    const htmlResponse =`
+        <html>
+            <head>
+                <title>Atlas Gym</title>
+            </head>
+            <body>
+                <h1>Bienvenido a la API de Atlas Gym</h1>
+            </body> 
+        </html>`
+        ;
+    res.send(htmlResponse);
+});
+
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
