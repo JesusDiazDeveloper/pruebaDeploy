e8c77fe7b73b7d4431468897fe6611385ce33eb6
Jesu <jesusdiaz013@gmail.com>
2024-07-07 15:32:13 -0300

    DB fixed

--- src/db/db.js
+++ src/db/db.js
@@ -6,21 +6,21 @@ const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config(); // Cargar las variables de entorno
 
-// const pool = mysql.createPool({
-//     host: process.env.MYSQL_ADDON_HOST,
-//     user: process.env.MYSQL_ADDON_USER,
-//     password: process.env.MYSQL_ADDON_PASSWORD,
-//     database: process.env.MYSQL_ADDON_DB,
-//     connectionLimit: 10
-// });
-
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'atlas_gym',
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
     connectionLimit: 10
 });
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'atlas_gym',
+//     connectionLimit: 10
+// });
 
 pool.getConnection().then(connection => {  
     console.log('Connected to MySQL database');
